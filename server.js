94b4996058a14e6c0e41c89acbeea46d8420922c
Alan <andrewsalan88@gmail.com>
2018-07-03 17:29:55 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+require("./app/routing/htmlRoutes")(app);
+
+
+// var PORT = process.env.PORT || 3306;
+
+app.listen(3000);
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World');
+// });
+
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./app/public/home.html"));
+//   });
+  
+
+// app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+//  });
+
+
+
+ 
+
+  
